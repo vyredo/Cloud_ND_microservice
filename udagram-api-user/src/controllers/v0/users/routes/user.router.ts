f6e2aea20bdd41e75943fa8bf33fc48bb36c41aa
alfredov@garena.com <alfredov@garena.com>
2020-12-03 21:12:16 +0700

    update get user.router.ts

--- udagram-api-user/src/controllers/v0/users/routes/user.router.ts
+++ udagram-api-user/src/controllers/v0/users/routes/user.router.ts
@@ -7,7 +7,9 @@ const router: Router = Router();
 
 router.use('/auth', AuthRouter);
 
-router.get('/');
+router.get('/', (req, res) => {
+  res.send('hello users')
+});
 
 router.get('/:id', async (req: Request, res: Response) => {
   const {id} = req.params;
