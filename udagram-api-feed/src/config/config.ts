3f6c4e45f34c9c33241a51f9f67c74a56bd82bbb
alfredov@garena.com <alfredov@garena.com>
2020-12-05 06:25:27 +0700

    modify config db for api-user and api-feed

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,12 +1,12 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': process.env.DB_USERNAME,
+  'password': process.env.DB_PASSWORD,
+  'database': process.env.DB_NAME,
+  'host': process.env.DB_HOST,
   'dialect': 'postgres',
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
+  'aws_media_bucket': process.env.AWS_MEDIA_BUCKET,
   'url': process.env.URL,
   'jwt': {
     'secret': process.env.JWT_SECRET,
